041d62796aadda6f65adbf1f4ea6215d7c1053ea
MaheshN1821 <maheshmahi18042004@gmail.com>
2026-03-01 17:57:48 +0530

    deployment bug fix -2

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,7 +1,7 @@
 from sqlalchemy import Column, Integer, String, Float, DateTime, ForeignKey, Text, Boolean
 from sqlalchemy.orm import relationship
 from datetime import datetime, timezone
-from database import Base
+from .database import Base
 
 class Medicine(Base):
     __tablename__ = "medicines"
